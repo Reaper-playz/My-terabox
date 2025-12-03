8beadf78912ce4434ec718f74a11a92e67d8f20b
Reaper-playz <gjxitdt222@gmail.com>
2025-12-03 23:47:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+import os
+
+BOT_TOKEN = os.environ.get("8406520934:AAG6DBUXj-3Wye7dJAbfTGU_FjmOYeRfcDM", "8406520934:AAG6DBUXj-3Wye7dJAbfTGU_FjmOYeRfcDM")
+API_ID = int(os.environ.get("35463925", "35463925"))
+API_HASH = os.environ.get("ce55193ef9d3f6894cdf61a268947c09", "ce55193ef9d3f6894cdf61a268947c09")
+COOKIE = os.environ.get("COOKIE", "YOUR_TERABOX_COOKIE_HERE")  # optional
